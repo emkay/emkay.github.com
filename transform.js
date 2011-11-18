2c27acc95548a3862882185863e5067122048a63
Michael <michael@lambdaphant.com>
2011-11-18 13:13:34 -0800

    adding fix for firefox

--- transform.js
+++ transform.js
@@ -2,6 +2,6 @@ var cat = document.getElementById('cat'),
     i   = 0;
 
 setInterval(function () {
-    cat.attributes.style.value = '-webkit-transform: rotate('+i+'deg);';
+    cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
     i += 1;
 }, 10);
