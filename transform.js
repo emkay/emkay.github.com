4c788ccdeead20a21c7a5b376df587f612fc956f
Michael <michael@lambdaphant.com>
2011-11-18 18:49:03 -0800

    adding reverse and changing start to forward

--- transform.js
+++ transform.js
@@ -1,21 +1,33 @@
-var cat   = document.getElementById('cat'),
-    start = document.getElementById('start'),
-    stop  = document.getElementById('stop'),
-    id    = null,
-    i     = 0;
+var cat     = document.getElementById('cat'),
+    forward = document.getElementById('forward'),
+    reverse = document.getElementById('reverse'),
+    stop    = document.getElementById('stop'),
+    id      = null,
+    i       = 0;
 
 
 
 if (document.addEventListener) {
-    start.addEventListener('click', function() {
-        if (id === null) {
-            var interval = document.getElementById('interval');
-            console.log(parseInt(interval.value, 10));
-            id = setInterval(function () {
-                cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
-                i += 1;
-            }, parseInt(interval.value, 10));
+    forward.addEventListener('click', function() {
+        if (id !== null) {
+            clearInterval(id);
         }
+        var interval = document.getElementById('interval');
+        id = setInterval(function () {
+            cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
+            i += 1;
+        }, parseInt(interval.value, 10));
+    });
+
+    reverse.addEventListener('click', function() {
+        if (id !== null) {
+            clearInterval(id); 
+        }
+        var interval = document.getElementById('interval');
+        id = setInterval(function () {
+            cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
+            i -= 1;
+        }, parseInt(interval.value, 10));
     });
 
     stop.addEventListener('click', function() {
