b5103cca9bb7baa0e6d079b9f65e4f9b2315c79f
Michael <michael@lambdaphant.com>
2011-11-18 19:03:57 -0800

    refactoring a bit

--- transform.js
+++ transform.js
@@ -5,31 +5,32 @@ var cat     = document.getElementById('cat'),
     id      = null,
     i       = 0;
 
+var rotator = function (direction) { 
+    return function () {
+        cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
+        switch (direction) {
+        case 'backward':
+            i -= 1;
+            break;
+        default:
+            i += 1;
+        }
+    };
+};
 
-
-if (document.addEventListener) {
-    forward.addEventListener('click', function() {
+var eventCallbackMaker = function (direction) {
+    return function () {
         if (id !== null) {
             clearInterval(id);
         }
         var interval = document.getElementById('interval');
-        id = setInterval(function () {
-            cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
-            i += 1;
-        }, parseInt(interval.value, 10));
-    });
+        id = setInterval(rotator(direction), parseInt(interval.value, 10));
+    };
+};
 
-    reverse.addEventListener('click', function() {
-        if (id !== null) {
-            clearInterval(id); 
-        }
-        var interval = document.getElementById('interval');
-        id = setInterval(function () {
-            cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
-            i -= 1;
-        }, parseInt(interval.value, 10));
-    });
-
+if (document.addEventListener) {
+    forward.addEventListener('click', eventCallbackMaker('forward'));
+    reverse.addEventListener('click', eventCallbackMaker('backward'));
     stop.addEventListener('click', function() {
         clearInterval(id);
         id = null;
