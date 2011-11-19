2759ee1e99186675f68c814554c4b4c215f89499
Michael <michael@lambdaphant.com>
2011-11-18 16:46:43 -0800

    EVENTS

--- transform.js
+++ transform.js
@@ -1,7 +1,25 @@
-var cat = document.getElementById('cat'),
-    i   = 0;
+var cat   = document.getElementById('cat'),
+    start = document.getElementById('start'),
+    stop  = document.getElementById('stop'),
+    id    = null,
+    i     = 0;
 
-setInterval(function () {
-    cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
-    i += 1;
-}, 10);
+
+
+if (document.addEventListener) {
+    start.addEventListener('click', function() {
+        if (id === null) {
+            var interval = document.getElementById('interval');
+            console.log(parseInt(interval.value, 10));
+            id = setInterval(function () {
+                cat.attributes.style.value += '-moz-transform: rotate('+i+'deg); -webkit-transform: rotate('+i+'deg);';
+                i += 1;
+            }, parseInt(interval.value, 10));
+        }
+    });
+
+    stop.addEventListener('click', function() {
+        clearInterval(id);
+        id = null;
+    });
+}
